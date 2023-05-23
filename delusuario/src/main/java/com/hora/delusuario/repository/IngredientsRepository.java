a0456fd9e9b6395d1987b52bc316969332dbdd83
MarioMedina <mmedina@excelsis.com.py>
2023-05-23 15:17:26 -0400

    se crea obteneringredientes y editaringredientes

--- delusuario/src/main/java/com/hora/delusuario/repository/IngredientsRepository.java
+++ delusuario/src/main/java/com/hora/delusuario/repository/IngredientsRepository.java
@@ -4,4 +4,5 @@ import com.hora.delusuario.model.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IngredientsRepository extends JpaRepository<ProductEntity, Integer> {
+    ProductEntity findById(int id);
 }
