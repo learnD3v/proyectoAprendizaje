0d133782ac1dee2ce6cd1023dfb8366f99595ad6
Alejandro Cabañas <acabanas@excelsis.com.py>
2023-05-24 08:18:51 -0400

    se añade front

--- delusuario/src/main/java/com/hora/delusuario/controller/AppApplication.java
+++ delusuario/src/main/java/com/hora/delusuario/controller/AppApplication.java
@@ -1,4 +1,4 @@
-package com.hora.delusuario;
+package com.hora.delusuario.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ delusuario/src/main/java/com/hora/delusuario/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.hora.delusuario.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("")
+    public String login(){
+        return "index";
+    }
+}
